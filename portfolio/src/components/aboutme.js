2d521ce9dcd2fd8dd9cadd8785f74f9705a880d5
Sandrine <nunubabila@gmail.com>
2021-01-27 18:42:40 -0800

    abouMe page

--- portfolio/src/components/aboutme.js
+++ portfolio/src/components/aboutme.js
@@ -1,48 +1,52 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+
 import prof from '../images/prof.jpeg';
 
 
 class About extends Component {
   render() {
     return(
-     
-      <Cell className='resume-right-col' row={4}>
-            <div style={{ textAlign: 'center' }}>
-              <img
-                src={prof}
-                alt='avatar'
-                style={{ height: '400px' }}
-              />
-            </div>
-
-            <h2 style={{ paddingTop: '2em' }}>Sandrine Nubia</h2>
-            <h4 style={{ color: 'grey' }}>Software Developer</h4>
-            <hr style={{ borderTop: '3px solid #ffb3ec', width: '70%' }} />
-            <p>
-              Certified Full-stack software developper with a solid background
-              in Security and Privacy compliance. Forward-thinking business
-              professional with diverse experience in sales, marketing, data
-              analysis, and continuous process improvement. Quick study with
-              history of success uncovering business needs, providing
-              data-supported insights, and offering strategies to solve complex
-              business challenges. Expertise building and fostering
-              relationships to drive internal and external stakeholder
-              experience through optimal communication skills. Accomplished as
-              team leader and individual contributor.{' '}
+      <div className='contact-body'>
+        <Grid className='contact-grid'>
+          <Cell col={6}>
+            <h2>Sandrine Nubia</h2>
+            <img src={prof} alt='avatar' style={{ height: '250px' }} />
+            <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since
             </p>
-            <hr style={{ borderTop: '3px solid #ffb3ec', width: '50%' }} />
-            <h5>Address</h5>
-            <p>1601 S Lane St, Seattle, Washington</p>
-            <h5>Phone</h5>
-            <p>(425) 435-5139</p>
-            <h5>Email</h5>
-            <p>sandrinenubia@gmail.com</p>
-
-            <hr style={{ borderTop: '3px solid #ffb3ec', width: '50%' }} />
           </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr />
 
-      
+            <div className='contact-list'>
+              <List>
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: '30px', fontFamily: 'Anton' }}
+                  >
+                    <i className='fa fa-phone-square' aria-hidden='true' />
+                    (425) 435-5139
+                  </ListItemContent>
+                </ListItem>
+
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: '30px', fontFamily: 'Anton' }}
+                  >
+                    <i className='fa fa-envelope' aria-hidden='true' />
+                    nunubabila@gmail.com
+                  </ListItemContent>
+                </ListItem>
+              </List>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+
     )
   }
 }
